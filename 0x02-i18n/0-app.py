01a8e447f447dd9fd76ef0e397e4678aeead18dd
Godwin Essien <kinggodwin4u@yahoo.com>
2024-11-05 17:49:16 +0100

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """define route that renders home page
+    as index.html
+    """
     return render_template('index.html')
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
